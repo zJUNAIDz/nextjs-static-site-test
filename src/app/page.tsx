67a2fd7e6ec1b2baf3c41f9e9bf3988a50ba111a
zJUNAIDz <junaidshaikh3967@gmail.com>
2024-11-21 23:25:20 +0530

    add main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+"use client"
+import { Company } from "@/components/company";
+import { Contact } from "@/components/contact";
+import { Hero } from "@/components/Hero";
+import Navbar from "@/components/navbar";
+import { Portfolio } from "@/components/portfolio";
+import { Service } from "@/components/service";
+import React from "react";
 
-export default function Home() {
+function Home() {
+
+  const [activeSection, setActiveSection] = React.useState("Home");
+
+  const homeRef = React.useRef<HTMLElement>(null);
+  const serviceRef = React.useRef<HTMLElement>(null);
+  const portfolioRef = React.useRef<HTMLElement>(null);
+  const companyRef = React.useRef<HTMLElement>(null);
+  const contactRef = React.useRef<HTMLElement>(null);
+
+  const onChangeHandler = (section: string) => {
+    console.log(section);
+    setActiveSection(section);
+    console.log(section);
+
+    switch (section) {
+      case "Home":
+        console.log(homeRef.current)
+        homeRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case "Service":
+        console.log(serviceRef.current)
+        serviceRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case "Portfolio":
+        console.log(portfolioRef.current)
+        portfolioRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case "Company":
+        companyRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case "About":
+        contactRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case "Contact":
+        contactRef.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+    <div className="flex flex-col w-screen">
+      <div className="sticky top-0 z-50 bg-white shadow-sm w-screen">
+        <Navbar
+          activeSection={activeSection} onChange={onChangeHandler}
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <Hero
+        ref={homeRef}
+      />
+      <Service
+        ref={serviceRef}
+      />
+      <Portfolio
+        ref={portfolioRef}
+      />
+      <Company
+        ref={companyRef}
+      />
+      <Contact
+        ref={contactRef}
+      />
+      <div className="text-[#B16B23] py-10 flex justify-center items-center">
+        Copyright&amp;2024 All Right Reserved
+      </div>
     </div>
-  );
+  )
 }
+
+export default Home;
