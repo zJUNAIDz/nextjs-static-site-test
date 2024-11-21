db4269bbf7e8f53c57daacc01c532d0e6efba9f2
zJUNAIDz <junaidshaikh3967@gmail.com>
2024-11-21 15:49:20 +0530

    add carousel data

---
+++ src/lib/carouselData.ts
@@ -0,0 +1,44 @@
+export const carouselItems = [
+  {
+    id: 1,
+    image: "/house1.png",
+    title: "Title 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt sint quaerat eligendi nisi in dignissimos necessitatibus explicabo ex ipsa magni dolore numquam, ipsum quos alias ut nobis aliquid. Obcaecati.",
+  },
+  {
+    id: 2,
+    image: "/house2.png",
+    title: "Title 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt sint quaerat eligendi nisi in dignissimos necessitatibus explicabo ex ipsa magni dolore numquam, ipsum quos alias ut nobis aliquid. Obcaecati.",
+  },
+  {
+    id: 3,
+    image: "/house3.png",
+    title: "Title 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt sint quaerat eligendi nisi in dignissimos necessitatibus explicabo ex ipsa magni dolore numquam, ipsum quos alias ut nobis aliquid. Obcaecati.",
+  },
+  {
+    id: 4,
+    image: "/house1.png",
+    title: "Title 4",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt sint quaerat eligendi nisi in dignissimos necessitatibus explicabo ex ipsa magni dolore numquam, ipsum quos alias ut nobis aliquid. Obcaecati.",
+  },
+  {
+    id: 5,
+    image: "/house2.png",
+    title: "Title 5",
+    description:
+      "DLorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt sint quaerat eligendi nisi in dignissimos necessitatibus explicabo ex ipsa magni dolore numquam, ipsum quos alias ut nobis aliquid. Obcaecati.",
+  },
+  {
+    id: 6,
+    image: "/house3.png",
+    title: "Title 6",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus sunt sint quaerat eligendi nisi in dignissimos necessitatibus explicabo ex ipsa magni dolore numquam, ipsum quos alias ut nobis aliquid. Obcaecati.",
+  }
+];
